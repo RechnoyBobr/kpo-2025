fa5297209d948c0f32152e0013457f1707397348
Ilya Shchepetkin <zhao.94@mail.ru>
2025-05-20 02:04:25 +0300

    feat: Init project

---
+++ practice/big-hw-2/APIGateway/src/main/java/hse/ApiGateway.java
@@ -0,0 +1,10 @@
+package hse;
+
+import org.springframework.boot.SpringApplication;
+
+public class ApiGateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateway.class, args);
+    }
+}
